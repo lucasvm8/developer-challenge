2e967c92f2270f63df47df39813577acf75dba28
Lucas Vasconcelos <lucasvm8@gmail.com>
2022-03-24 08:09:32 -0300

    mongoose change

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://root:xncaqNNerKIc898V@cluster0.ja4j9.mongodb.net/test'),
+      ''),
     UsersModule,
   ],
   controllers: [],
